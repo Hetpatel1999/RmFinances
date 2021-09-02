ef9db1ebf3c6bb096b214475164a6491a8c382ae
Hetpatel1999 <HetPatelpatelhet1999@gmail.com>
2021-09-02 11:26:41 +0530

    Project structure

---
+++ JQGrid/src/com/GridDao/GridDao.java
@@ -0,0 +1,45 @@
+package com.GridDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.been.GridModel;
+
+public class GridDao {
+
+	public List<GridModel> getdata() {
+		try
+		{
+			 Class.forName("com.mysql.jdbc.Driver");
+
+		     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/het","root","root");
+		     PreparedStatement prepared = con.prepareStatement("Select * from student");
+		     ResultSet rs = prepared.executeQuery();
+		     
+		     List<GridModel> list=new ArrayList<>();
+				while(rs.next())
+				{
+					GridModel mo=new GridModel();
+					mo.setId(rs.getInt("id"));
+					mo.setName(rs.getString("name"));
+					mo.setAge(rs.getString("age"));
+					mo.setLocation(rs.getString("location"));
+					
+					list.add(mo);
+				}
+		 
+		
+		 prepared.close();
+		 con.close();
+		 return list;
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
